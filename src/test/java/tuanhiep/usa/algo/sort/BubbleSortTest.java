9267a8e635f2ce5dcb7c3df83cde6cd17848ef41
Tuan-Hiep TRAN <tuanhiep.tran88@gmail.com>
2018-09-22 00:23:19 +0200

    Add second test case Bubble Sort

--- src/test/java/tuanhiep/usa/algo/sort/BubbleSortTest.java
+++ src/test/java/tuanhiep/usa/algo/sort/BubbleSortTest.java
@@ -12,4 +12,13 @@ public class BubbleSortTest {
         bubbleSort.test(new ArrayList());
         bubbleSort.print();
     }
+    @Test
+    public void bubbleSortTest() {
+        bubbleSort.setUpDefault();
+        ArrayList parameters= new ArrayList();
+        int[] input = new int[]{9,1,7,3,8,1,78,33,54,11,13,15,63,95,4235,315663,3515,15,535131,1,1113513};
+        parameters.add(input);
+        bubbleSort.test(parameters);
+        bubbleSort.print();
+    }
 }
