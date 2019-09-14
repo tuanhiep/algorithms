e95f91bdb60c706e2669fe3cbef3b633d7a81635
Tuan Hiep TRAN <tuanhiep.tran88@gmail.com>
2019-09-13 23:45:59 -0600

    finish setUpDefault() function

--- src/main/java/tuanhiep/usa/algo/others/CoinChange.java
+++ src/main/java/tuanhiep/usa/algo/others/CoinChange.java
@@ -1,17 +1,34 @@
 package tuanhiep.usa.algo.others;
 
 import tuanhiep.usa.common.abstractAlgo;
+import tuanhiep.usa.common.utils.TypeAlgo;
+
+import java.util.ArrayList;
 
 public class CoinChange extends abstractAlgo {
 
 
     @Override
     public void setUpDefault() {
+        ArrayList parametersDefault = new ArrayList();
+        int total=27;
+        int[] denominations = {25, 10, 5, 1};
+        parametersDefault.add(total);
+        parametersDefault.add(denominations);
+        this.setParameters(parametersDefault);
+        this.setName("Algorithm solves Coin Change problem");
+        this.setType(TypeAlgo.OTHERS);
+        this.setDescription("We use algorithm to solve the problem of making change with a number of predefined denominations ");
 
     }
 
     @Override
     public void run() {
+        int total = (int) this.getParameters().get(0);
+        int[] denominations = (int[]) this.getParameters().get(1);
+
+
+
 
     }
 
--- src/main/java/tuanhiep/usa/common/utils/TypeAlgo.java
+++ src/main/java/tuanhiep/usa/common/utils/TypeAlgo.java
@@ -4,7 +4,8 @@ public enum TypeAlgo {
     SEARCH("search"),
     CLASSIFY("classify"),
     SORT("sort"),
-    STRUCTURE("data structure");
+    STRUCTURE("data structure"),
+    OTHERS("others");
     private String type;
 
     TypeAlgo(String s) {
