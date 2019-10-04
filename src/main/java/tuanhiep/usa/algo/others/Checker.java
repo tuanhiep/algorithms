eb7886475209dd072188c858c8f30ef1ac99d876
Tuan Hiep TRAN <tuanhiep.tran88@gmail.com>
2019-10-04 17:45:08 -0600

    use comparator in java

---
+++ src/main/java/tuanhiep/usa/algo/others/Checker.java
@@ -0,0 +1,32 @@
+package tuanhiep.usa.algo.others;
+
+import java.util.Comparator;
+
+public class Checker implements Comparator<Player> {
+
+    @Override
+    public int compare(Player p1, Player p2) {
+        if (p1.score == p2.score) {
+            return p1.name.compareTo(p2.name);
+        }
+        if (p1.score > p2.score) {
+            return 1;
+        } else {
+            return -1;
+        }
+
+
+    }
+}
+
+class Player {
+    public double score;
+    public String name;
+
+    public Player(double score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+
+}
