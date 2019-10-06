aa46be17bdf3942d86422a0b1c5f298dfacf5f1f
Tuan Hiep TRAN <tuanhiep.tran88@gmail.com>
2019-10-06 17:22:15 -0600

    Solve the problem of a little kid jump staircase with leap of 1 or 2 or 3 steps. The staircase has 100 steps overall

---
+++ src/main/java/tuanhiep/usa/algo/others/RecursiveStairCase.java
@@ -0,0 +1,25 @@
+package tuanhiep.usa.algo.others;
+
+import tuanhiep.usa.common.abstractAlgo;
+
+public class RecursiveStairCase extends abstractAlgo {
+    @Override
+    public void setUpDefault() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public Object getResult() {
+        return null;
+    }
+
+    @Override
+    public void print() {
+
+    }
+}
