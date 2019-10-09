6805509300452ccc5b60cdb5b3736410db0562b6
bigname <tuanhiep.tran88@gmail.com>
2019-10-08 21:40:09 -0600

    Find special Pythagorean triplet with given sum

---
+++ src/main/java/tuanhiep/usa/algo/euler/SpecialPythagoreanTriplet.java
@@ -0,0 +1,22 @@
+package tuanhiep.usa.algo.euler;
+
+public class SpecialPythagoreanTriplet {
+    public static void main(String[] args) {
+        System.out.println(findTriplet(1000));
+
+    }
+
+    public static long findTriplet(int sum) {
+        for (int a = 1; a < sum; a++) {
+            for (int b = 1; b < sum; b++) {
+                if ((2 * sum * (a + b) - 2 * a * b) == Math.pow(sum, 2)) {
+                    int c= (int) Math.pow(a*a+b*b,0.5);
+                    return a*b*c;
+                }
+            }
+        }
+        return -1;
+    }
+
+
+}
