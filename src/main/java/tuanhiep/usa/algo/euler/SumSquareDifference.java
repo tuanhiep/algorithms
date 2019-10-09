8fde0ef7bb885867efb24eb1a9e29400c97dbe29
bigname <tuanhiep.tran88@gmail.com>
2019-10-08 19:59:37 -0600

    Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.

---
+++ src/main/java/tuanhiep/usa/algo/euler/SumSquareDifference.java
@@ -0,0 +1,20 @@
+package tuanhiep.usa.algo.euler;
+
+public class SumSquareDifference {
+    public static void main(String[] args) {
+        System.out.println(findSumSquareDifference(100));
+    }
+
+    public static long findSumSquareDifference(int n) {
+        long sum=0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = i+1; j <=  n; j++) {
+                if (i!=j) {
+                    sum+=2*i*j;
+                }
+            }
+        }
+        return sum;
+
+    }
+}
