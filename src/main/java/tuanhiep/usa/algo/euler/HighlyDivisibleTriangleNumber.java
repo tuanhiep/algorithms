a2d7a9dad10e29c06278576e886c1b8bc3f8367c
bigname <tuanhiep.tran88@gmail.com>
2019-10-09 19:36:24 -0600

    Algorithm to find the triangle number which has over 500 divisors

---
+++ src/main/java/tuanhiep/usa/algo/euler/HighlyDivisibleTriangleNumber.java
@@ -0,0 +1,51 @@
+package tuanhiep.usa.algo.euler;
+
+import java.util.HashMap;
+
+public class HighlyDivisibleTriangleNumber {
+    public static void main(String[] args) {
+        System.out.println(findTriangleNumber(500));
+    }
+
+   public static HashMap<Integer, Integer> memo = new HashMap<>();
+
+    public static long findTriangleNumber(int n) {
+
+        int countDivisor = 0;
+        int position = 2;
+        while (countDivisor <= n) {
+            if (position % 2 == 0) {
+                countDivisor = getNumberDivisor(position / 2) * getNumberDivisor(position + 1);
+
+            } else {
+                countDivisor = getNumberDivisor((position + 1) / 2) * getNumberDivisor(position);
+
+            }
+            position++;
+
+        }
+        return generateTriangleNumner(position - 1);
+    }
+
+    private static int getNumberDivisor(int number) {
+        if (memo.containsKey(number)) {
+            return memo.get(number);
+        }
+        int count = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        memo.put(number, count);
+        return count;
+    }
+
+
+    private static long generateTriangleNumner(int position) {
+        return (position + 1) * position / 2;
+
+    }
+
+
+}
