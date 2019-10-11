4fd3823da7f08742f2b49dd5f1a4f96365528769
Tuan Hiep TRAN <tuanhiep.tran88@gmail.com>
2019-10-11 13:51:52 -0600

    Algorithm to find the Sunday is also the first day of month from 1 Jan 1901 to 31 Dec 2000

--- src/main/java/tuanhiep/usa/algo/euler/CountingSunday.java
+++ src/main/java/tuanhiep/usa/algo/euler/CountingSunday.java
@@ -2,7 +2,86 @@ package tuanhiep.usa.algo.euler;
 
 public class CountingSunday {
     public static void main(String[] args) {
+        System.out.println(findSumSunday(1901, 2000));
+    }
 
+    private static long findSumSunday(int startYear, int endYear) {
+        long sum = 0;
+        for (int i = startYear; i <= endYear; i++) {
+            sum += findSumSunday(i);
+        }
+        return sum;
+    }
+
+    private static long findSumSunday(int year) {
+        long sum = 0;
+        long[] indexFirstDays = getIndexFirstDayOfMonth(year, isLeapYear(year));
+        for (long index : indexFirstDays) {
+            if (index % 7 == 0) {
+                sum++;
+            }
+        }
+        return sum;
+
+    }
+
+    private static long[] getIndexFirstDayOfMonth(int year, boolean leapYear) {
+        long[] indexes = new long[12];
+        if (year < 1900) return null;
+        long index = getIndexOfFirstDayYear(year);
+        int i = 0;
+        indexes[i] = index;
+        i++;
+        indexes[i] = indexes[i - 1] + 31;// January
+        i++;
+        if (isLeapYear(year)) {
+            indexes[i] = indexes[i - 1] + 29;// February
+        } else {
+            indexes[i] = indexes[i - 1] + 28;
+        }
+        i++;
+        indexes[i] = indexes[i - 1] + 31;// Mars
+        i++;
+        indexes[i] = indexes[i - 1] + 30;// April
+        i++;
+        indexes[i] = indexes[i - 1] + 31;// May
+        i++;
+        indexes[i] = indexes[i - 1] + 30;// June
+        i++;
+        indexes[i] = indexes[i - 1] + 31;// July
+        i++;
+        indexes[i] = indexes[i - 1] + 31;// August
+        i++;
+        indexes[i] = indexes[i - 1] + 30;// September
+        i++;
+        indexes[i] = indexes[i - 1] + 31;// October
+        i++;
+        indexes[i] = indexes[i - 1] + 30;// November
+
+        return indexes;
+
+    }
+
+    private static long getIndexOfFirstDayYear(int year) {
+        if (year < 1900) return -1;
+        int index = 1;
+        for (int i = 1900; i < year; i++) {
+            if (isLeapYear(i)) {
+                index += 366;
+            } else {
+                index += 365;
+            }
+        }
+        return index;
+
+    }
+
+    private static boolean isLeapYear(int year) {
+        if (year % 100 == 0) {
+            return year % 400 == 0;
+        } else {
+            return year % 4 == 0;
+        }
     }
 
 
