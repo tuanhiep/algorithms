e1d2443c64e35904bfc59d5f3d8793a08c379c82
Tuan Hiep TRAN <tuanhiep.tran88@gmail.com>
2019-10-23 18:46:20 -0600

    Problem 40 solved: Find product of set of nth digit of chain

--- src/main/java/tuanhiep/usa/algo/euler/Problem40_ChampernowneConstant.java
+++ src/main/java/tuanhiep/usa/algo/euler/Problem40_ChampernowneConstant.java
@@ -1,7 +1,30 @@
 package tuanhiep.usa.algo.euler;
 
 public class Problem40_ChampernowneConstant {
+    public static String chain="0";
+    public static int index=0;
+
     public static void main(String[] args) {
+
+        System.out.println(getDigit(1));
+        System.out.println(getDigit(2));
+        System.out.println(getDigit(3));
+        System.out.println(getDigit(11));
+        System.out.println(getDigit(12));
+        System.out.println(getDigit(1000));
+        System.out.println(findValue());
+    }
+
+    private static int findValue() {
+        return getDigit(1)*getDigit(10)*getDigit(100)*getDigit(1000)*getDigit(10000)*getDigit(100000)*getDigit(1000000);
+    }
+
+    private static int getDigit(int position) {
+        while (position >= chain.length()) {
+            index++;
+            chain += String.valueOf(index);
+        }
+        return Character.getNumericValue(chain.charAt(position));
 
     }
 }
